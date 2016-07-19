7af0ba77c46a80384541c497a47eb03d460660d7
Dong Liu <edongliu@gmail.com>
2016-07-19 11:10:35 -0400

    add config

---
+++ config/config.js
@@ -0,0 +1,25 @@
+/**
+ * @fileOverview load the configuration json files
+ */
+
+var fs = require('fs');
+var path = require('path');
+
+var ad = require('./ad.json');
+var ldap = require('./ldap.json');
+var app = require('./app.json');
+var auth = require('./auth.json');
+var mongo = require('./mongo.json');
+var redis = require('./redis.json');
+
+
+app.log_dir = path.resolve(__dirname, app.log_dir || '../logs/');
+
+
+module.exports = {
+  ad: ad,
+  app: app,
+  auth: auth,
+  mongo: mongo,
+  redis: redis
+};
