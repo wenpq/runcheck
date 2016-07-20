a875b435e2e38040580ce310cd0c508bd5758393
Dong Liu <edongliu@gmail.com>
2016-07-20 17:15:22 -0400

    add login logout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
-var router = express.Router();
+var index = express.Router();
+var auth = require('../lib/auth');
+var log = require('../lib/log');
+var config = require('../config/config');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+index.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+index.get('/login', auth.ensureAuthenticated, function(req, res) {
+  if (req.session.userid) {
+    return res.redirect('/');
+  }
+  // something wrong
+  res.status(400).send('please enable cookie in your browser');
+});
+
+index.get('/logout', function (req, res) {
+  if (req.session) {
+    req.session.destroy(function (err) {
+      if (err) {
+        log.error(err);
+      }
+    });
+  }
+
+  res.redirect(config.auth.cas + '/logout');
+});
+
+module.exports = index;
