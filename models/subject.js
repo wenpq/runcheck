58a6403d64d943d16df61874b15dddde89f8371a
Dong Liu <edongliu@gmail.com>
2016-07-25 09:31:45 -0400

    add subject

---
+++ models/subject.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+/*******
+ * A subject is a unit or a cross-unit topic, and the leader has its
+ * authorization. The admin maintains it for the authorization control.
+ * name: the short name
+ * fullName: the long name
+ * leader: the leader's AD id
+ *******/
+var subject = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  fullName: {
+    type: String,
+    unique: true,
+    trim: true
+  },
+  leader: {
+    type: String,
+    lowercase: true,
+    trim: true
+  }
+});
+
+var Subject = mongoose.model('Subject', subject);
+module.exports = {
+  Subject: Subject
+};
