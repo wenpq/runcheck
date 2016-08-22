95378f5f2524d2e7dceb5e5ac55e25dc83854c2f
Dong Liu <edongliu@gmail.com>
2016-08-22 15:39:03 -0400

    add prepend and eslint

--- public/javascripts/history.js
+++ public/javascripts/history.js
@@ -4,6 +4,8 @@
  *
  *
  ******/
+/*global moment:false, changeTemplate:false*/
+
 var History = (function (parent, $) {
 
   function renderHistory(updates, selector) {
@@ -19,7 +21,26 @@ var History = (function (parent, $) {
     }
   }
 
+  function prependHistory(updates, selector) {
+    selector = typeof selector === 'undefined' ? '#history' : selector;
+    var changeIds = [];
+    $(selector).children('.change').each(function () {
+      changeIds.push($(this).prop('id'));
+    });
+    if (updates && updates.length > 1) {
+      updates.forEach(function (u) {
+        if (changeIds.indexOf(u._id) === -1) {
+          $(selector).prepend(changeTemplate({
+            h: u,
+            moment: moment
+          }));
+        }
+      });
+    }
+  }
+
   parent.renderHistory = renderHistory;
+  parent.prependHistory = prependHistory;
 
   return parent;
 
